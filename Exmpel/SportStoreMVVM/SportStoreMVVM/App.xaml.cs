0a93cc9f02e885b2fa599d711e84a12027f6bac0
PDN <ponomarevd61@gmail.com>
2023-11-01 19:58:49 +0300

    1.0

---
+++ Exmpel/SportStoreMVVM/SportStoreMVVM/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using SportStoreMVVM.ViewModels;
+using SportStoreMVVM.Views;
+
+namespace SportStoreMVVM
+{
+    
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            LoginWindow lw = new LoginWindow();
+
+            //MainWindow mv = new MainWindow();
+
+            LoginViewModel vm = new LoginViewModel();
+            lw.DataContext = vm;
+
+            lw.Show();
+        }
+
+    }
+}
---
+++ Exmpel/SportStoreMVVM/SportStoreMVVM/Models/RelatedProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SportStoreMVVM.Models;
+
+public partial class RelatedProduct
+{
+    public int Id { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int RelatedProductId { get; set; }
+
+
+
+    public virtual Product RelatedProductNav { get; set; } = null!;
+    public virtual Product Product { get; set; } = null!;
+
+    
+}
